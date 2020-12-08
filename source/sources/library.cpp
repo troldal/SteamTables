e00ce1cde7ec6342f51d2829bcf1b3aa7231b97e
troldal <kenneth.balslev@gmail.com>
2020-12-08 20:35:39 +0100

    First commit

--- source/sources/library.cpp
+++ source/sources/library.cpp
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "../headers/library.hpp"
 
 #include <iostream>
 
--- tests/Test.cpp
+++ tests/Test.cpp
@@ -11,34 +11,12 @@
 #define CATCH_CONFIG_RUNNER
 
 #include "catch.hpp"
-#include <OpenXLSX.hpp>
 #include <cstdio>
-
-using namespace OpenXLSX;
-
-void PrepareDocument(std::string name)
-{
-    XLDocument doc;
-    std::remove(name.c_str());
-    doc.create(name);
-    doc.save();
-    doc.close();
-}
 
 int main(int argc, char* argv[])
 {
     // Global Setup
-    XLDocument doc;
-
-    std::remove("./TestDocumentCreation.xlsx");
-    std::remove("./TestDocumentCreationNew.xlsx");
-
-    PrepareDocument("./TestDocumentProperties.xlsx");
-    PrepareDocument("./TestWorkbook.xlsx");
-    PrepareDocument("./TestSheet.xlsx");
-    PrepareDocument("./TestWorksheet.xlsx");
-    PrepareDocument("./TestCellReference.xlsx");
-    PrepareDocument("./TestCell.xlsx");
+    // ...
     // Global Setup Complete
 
     // Run Test Suite
